eb27d619739087a55e9521706c634610795fd36a
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-28 23:02:52 +0530

    Create 7_5_Dequeue_using_Circular_queue.py

---
+++ 7_5_Dequeue_using_Circular_queue.py
@@ -0,0 +1,53 @@
+class Queue:
+    def __init__(self,maxSize):
+        self.items = maxSize * [None]
+        self.maxSize = maxSize
+        self.start = -1
+        self.top = -1
+        
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return " ".join(values)
+    
+    def isFull(self):
+        if self.top + 1 == self.start:
+            return True
+        elif self.start == 0 and self.top + 1 == self.maxsize:
+            return True
+        else: 
+            return False
+        
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+    
+    def enqueue(self,value):
+        if self.isFull():
+            return "The Queue is Full"
+        else:
+            if self.top + 1 == self.maxsize:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "The element is inserted at the end of the queue"
+    
+    def dequeue(self):
+        if self.isEmpty:
+            return "No element in the queue"
+        else:
+            firstElement = self.items[self.start]
+            start = self.start
+            if self.start == self.top:
+                self.start = -1
+                self.top = -1
+            elif self.start + 1 == self.maxsize:
+                self.start = 0
+            else:
+                self.start += 1
+            self.items[start] = None
+            return firstElement
